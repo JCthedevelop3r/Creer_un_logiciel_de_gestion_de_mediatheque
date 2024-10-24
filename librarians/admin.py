c5738031f4b0abaa65b9c0259f3dfbb457580f50
JIICE <minotjeanclaude60@gmail.com>
2024-10-24 13:27:24 +0100

    Allow creation of a new member

--- librarians/admin.py
+++ librarians/admin.py
@@ -5,7 +5,14 @@ from librarians.models_borrowing import Borrowing
 
 
 class MemberAdmin(admin.ModelAdmin):
-    list_display = ['user', 'date_of_birth', 'place_of_birth', 'city_of_residence', 'phone_number', 'borrowings_number']
+    list_display = ['user', 'get_user_email', 'date_of_birth', 'place_of_birth', 'city_of_residence', 'phone_number', 'borrowings_number']
+
+    # Méthode pour afficher l'email de l'utilisateur lié
+    def get_user_email(self, obj):
+        return obj.user.email
+
+    # Personnaliser l'en-tête de la colonne dans l'admin
+    get_user_email.short_description = 'Email'
 
 
 class MediaAdmin(admin.ModelAdmin):
--- librarians/views.py
+++ librarians/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-
+from django.contrib.auth.models import User
+from django.contrib import messages
 from .models_borrowing import Borrowing
 from .models_members import Member
 from .models_medias import Media
@@ -56,8 +57,39 @@ def add_media(request):
 
 
 def create_member(request):
-    context = {'name': 'create_member'}
-    return render(request, 'librarians/create_member.html', context)
+    if request.method == 'POST':
+        first_name = request.POST['first-name']
+        last_name = request.POST['last-name']
+        email = request.POST['e-mail']
+        date_of_birth = request.POST['date-of-birth']
+        place_of_birth = request.POST['place-of-birth']
+        city_of_residence = request.POST['city-of-residence']
+        phone_number = request.POST['phone-number']
+
+        if User.objects.filter(email=email).exists():
+            messages.error(request, "Un utilisateur avec cet e-mail existe déjà.")
+            return render(request, 'librarians/create_member.html')
+
+        user = User.objects.create(
+            first_name=first_name,
+            last_name=last_name,
+            email=email,
+            username=f"{first_name} {last_name}"
+        )
+
+        member = Member.objects.create(
+            user=user,
+            date_of_birth=date_of_birth,
+            place_of_birth=place_of_birth,
+            city_of_residence=city_of_residence,
+            phone_number=phone_number,
+            borrowings_number=0
+        )
+
+        messages.success(request, "Le membre a été créé avec succès.")
+        messages.error(request, "Une erreur s'est produite. Veuillez réessayer.")
+
+    return render(request, 'librarians/create_member.html')
 
 
 def display_members(request):
