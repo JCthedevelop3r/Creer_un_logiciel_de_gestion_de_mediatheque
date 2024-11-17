623b8329290c0677743b3ea6040a7dcbdc985f26
JIICE <minotjeanclaude60@gmail.com>
2024-11-17 22:12:16 +0100

    development of the test of the media addition functionality

--- librarians/urls.py
+++ librarians/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('create_borrowing', views.create_borrowing, name='create_borrowing'),
     path('return_borrowing', views.return_borrowing, name='return_borrowing'),
     path('display_medias', views.display_medias, name='display_medias'),
-    path('add_media', views.add_media),
+    path('add_media', views.add_media, name='add_media'),
     path('create_member', views.create_member, name='create_member'),
     path('display_members', views.display_members, name='display_members'),
     path('update_member', views.update_member, name='update_member'),
