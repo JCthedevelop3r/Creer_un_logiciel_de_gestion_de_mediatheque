73c76059f9453733e704b475ec1f632f7af7ea81
JIICE <minotjeanclaude60@gmail.com>
2024-11-24 17:29:41 +0100

    Development of the test of the media display functionality of the applicationn members

--- members/tests/test_display_medias.py
+++ members/tests/test_display_medias.py
@@ -43,33 +43,6 @@ def test_display_medias(client, multiple_medias):
     assert response.status_code == 200
 
     assert "Le Seigneur des anneaux" in response.content.decode()
-    assert "book" in response.content.decode()
     assert "Baby" in response.content.decode()
-    assert "cd" in response.content.decode()
     assert "Monopoly" in response.content.decode()
-    assert "board_game" in response.content.decode()
     assert "Le Roi lion" in response.content.decode()
-    assert "dvd" in response.content.decode()
-
-
-@pytest.mark.django_db
-def test_delete_media(client, multiple_medias):
-    response = client.get(reverse('display_medias'))
-    assert response.status_code == 200
-    assert "Le Seigneur des anneaux" in response.content.decode()
-    assert "book" in response.content.decode()
-
-    # Préparation du média à supprimer : "Le Seigneur des anneaux"
-    book = multiple_medias[0]
-    delete_data = {
-        'media-id': book.id,
-        'media-type': book.type,
-    }
-
-    # Envoi d'une requête POST simulant le clic sur le bouton "Supprimer"
-    response = client.post(reverse('display_medias'), delete_data, follow=True)
-    assert response.status_code == 200
-
-    # Vérification de l'abscence du média
-    response = client.get(reverse('display_medias'))
-    assert "Le Seigneur des anneaux" not in response.content.decode()
